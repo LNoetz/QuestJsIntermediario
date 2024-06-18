3f71b7285b6a3ac96ff6b20428891bff609b6987
Lucas Noetzold <lucasnoetzoldsantos@gmail.com>
2024-06-18 13:55:35 -0300

    refatoração da lógica no JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,22 @@
 const dadosUsuario = document.querySelectorAll('.camposdepreenchimento');
-const btnEnviarDados = document.getElementById('btn-enviar');
+const btnEnviarDados = document.querySelector('.btn-enviar');
 
+btnEnviarDados.addEventListener('click', (evento) => {
+    evento.preventDefault();
+    dadosUsuario.forEach((element) => {
+        if (element.value) {
+            element.classList.add('bordaverde')
+            element.nextElementSibling.classList.remove('mostrar')
+        } else {
+            element.classList.remove('bordaverde')
+            element.classList.add('bordavermelha')
+            element.nextElementSibling.classList.add('mostrar')
+        }
+    });
+});
+/*
 btnEnviarDados.addEventListener('click', (event) => {
     event.preventDefault();
-    let valido = true
     dadosUsuario.forEach(element => {
         if (element.value === "") {
             mostrarErro(element);
@@ -13,20 +26,18 @@ btnEnviarDados.addEventListener('click', (event) => {
             mostrarSucesso(element);
         }
     });
-})
-
+});
 function mostrarErro(element) {
     element.classList.add('bordavermelha');
     element.classList.remove('bordaverde');
     element.nextElementSibling.classList.add('mostrar');
     element.nextElementSibling.textContent = "campo obrigatório";
 };
-
 function esconderErro(element) {
     element.classList.remove('bordavermelha');
     element.nextElementSibling.classList.remove('mostrar');
 };
-
 function mostrarSucesso(element) {
     element.classList.add('bordaverde');
 };
+*/
